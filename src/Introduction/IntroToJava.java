581991d1e5d61e44696189fae83f97e88b160a52
Tanmay Arya <tanmayarya80@gmail.com>
2023-04-14 23:10:13 +0530

    Introduction

---
+++ src/Introduction/IntroToJava.java
@@ -0,0 +1,10 @@
+package Introduction;
+
+public class IntroToJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Newton School");
+	}
+
+}
